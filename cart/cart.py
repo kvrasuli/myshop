3286c6441dc2325ef4a1636ac44dfa9e66af6351
Kirill Rasuli <rasulikv@gmail.com>
2020-06-07 20:36:39 +0300

    cart items iterator fixed

--- cart/cart.py
+++ cart/cart.py
@@ -35,14 +35,13 @@ class Cart(object):
     def __iter__(self):
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
-
         cart = self.cart.copy()
         for product in products:
             cart[str(product.id)]['product'] = product
         for item in cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
-        yield item
+            yield item
 
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
