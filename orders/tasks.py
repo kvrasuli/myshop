07aa249759acb45057ff4af7609db4e7e1725cda
Kirill Rasuli <rasulikv@gmail.com>
2020-06-09 19:57:17 +0300

    celery async task added

---
+++ orders/tasks.py
@@ -0,0 +1,13 @@
+from celery import task
+from django.core.mail import send_mail
+from .models import Order
+
+@task
+def order_created(order_id):
+    """Задача отправки email уведомлений при успешном оформлении заказа"""
+    order = Order.objects.get(id=order_id)
+    subject = f'Order nr. {order.id}'
+    message = f'Dear {order.first_name},\n\n You have succesfully placed an order.\
+        Your order id is {order.id}.'
+    mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
+    return mail_sent
